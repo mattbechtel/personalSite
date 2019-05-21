844eafc0ead618fac2dc12cfa81fcc79fea6bdba
mattb <mattbechtel123@gmail.com>
2019-05-20 21:17:39 -0500

    Added bootstrap, made a simple nav bar with stubbed values

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: [
+        './dashboard.component.css',  
+        "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+    ]
 })
 export class DashboardComponent implements OnInit {
 
